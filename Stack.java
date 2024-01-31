a7931503aa62834cf02a6fa0dd0abe3bacccadd6
Gabriel Bran <bra23590@uvg.edu.gt>
2024-01-30 18:33:39 -0600

    Implementación Stack

---
+++ Stack.java
@@ -0,0 +1,27 @@
+public class Stack<T> implements UVGStack<T> {
+
+    @Override
+    public void push(T x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public int pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public int top() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'top'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+    
+}
